8f1726ff74a1bc3dc15fdeaaec3738ec931a3afd
Laura Parra <laupafer93@gmail.com>
2024-09-06 11:21:09 +0200

    exercise 4

--- web/src/services/api-movies.jsx
+++ web/src/services/api-movies.jsx
@@ -1,11 +1,15 @@
 // login
 
 const getMoviesFromApi = () => {
-  console.log('Se están pidiendo las películas de la app');
+  console.log("Se están pidiendo las películas de la app");
   // CAMBIA ESTE FETCH PARA QUE APUNTE A UN ENDPOINT DE TU SERVIDOR, PIENSA SI DEBE SER GET O POST, PIENSA QUÉ DATOS DEBES ENVIAR, ETC
-  return fetch('//beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json')
-    .then(response => response.json())
+  return fetch(
+    "https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/netflix-v1/empty.json"
+  )
+    .then((response) => response.json())
+
     .then(() => {
+    
       // CAMBIA EL CONTENIDO DE ESTE THEN PARA GESTIONAR LA RESPUESTA DEL SERVIDOR Y RETORNAR AL COMPONENTE APP LO QUE NECESITA
       return {
         success: true,
@@ -30,7 +34,7 @@ const getMoviesFromApi = () => {
 };
 
 const objToExport = {
-  getMoviesFromApi: getMoviesFromApi
+  getMoviesFromApi: getMoviesFromApi,
 };
 
 export default objToExport;
